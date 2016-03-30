2d3772c6c33984ad9ea1423ad14ca9f544442401
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-03-29 17:49:29 -0700

    travis deploy

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,4 @@
-ls -l
 cd dist
-ls -l
 git init
 
 git config user.name "Travis CI"
@@ -9,4 +7,4 @@ git config user.email "your@email.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-git push --force --quiet "https://${GH_TOKEN}@{GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
